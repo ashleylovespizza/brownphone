546a4b22e3be3367b366b53dbb729c3a4bbfb934
Ashley Holtgraver <aholtgraver@gmail.com>
2016-09-02 09:08:57 -0400

    feedreadin the day away

--- raspi/button.js
+++ raspi/button.js
@@ -1,1 +1,21 @@
-button.js
+var wpi = require('wiring-pi');
+var Sound = require('node-aplay');
+
+var testSound = new Sound('test.wav');
+console.log("started");
+
+// GPIO pin of the button
+var configPin = 7;
+
+wpi.setup('wpi');
+
+wpi.pinMode(configPin, wpi.INPUT);
+wpi.pullUpDnControl(configPin, wpi.PUD_UP);
+
+
+wpi.wiringPiISR(configPin, wpi.INT_EDGE_FALLING, function() {
+//  if (wpi.digitalRead(configPin)) {
+       	console.log("push");
+       	testSound.play();
+ // }
+});
