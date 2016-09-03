b8a64ba10880b2ab0360e016e016ced8ea79a7cc
Ashley Holtgraver <aholtgraver@gmail.com>
2016-09-03 15:40:00 +0000

    basic button

--- raspi/button.js
+++ raspi/button.js
@@ -29,17 +29,17 @@ var test = new Sound('test.wav');
 test.play();
 //
 //// GPIO pin of the button
-//var configPin = 7;
-//
-//wpi.setup('wpi');
-//
-//wpi.pinMode(configPin, wpi.INPUT);
-//wpi.pullUpDnControl(configPin, wpi.PUD_UP);
-//
-//
-//wpi.wiringPiISR(configPin, wpi.INT_EDGE_FALLING, function() {
-////  if (wpi.digitalRead(configPin)) {
-//       	console.log("push");
-//       	testSound.play();
-// // }
-//});//
+var configPin = 7;
+
+wpi.setup('wpi');
+
+wpi.pinMode(configPin, wpi.INPUT);
+wpi.pullUpDnControl(configPin, wpi.PUD_UP);
+
+
+wpi.wiringPiISR(configPin, wpi.INT_EDGE_FALLING, function() {
+//  if (wpi.digitalRead(configPin)) {
+       	console.log("push");
+       	test.play();
+ // }
+});
