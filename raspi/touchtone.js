bc92c7816f4e19e6e48ecb6eba81730dd62dc800
Ashley Holtgraver <aholtgraver@gmail.com>
2016-09-03 14:09:59 -0400

    pingrid 2d

--- raspi/touchtone.js
+++ raspi/touchtone.js
@@ -12,15 +12,44 @@ var row1Pin = 9
   , hangupPin = 8;
 
 
+var pinGrid = [ [0, 0, 0]
+ 			  , [0, 0, 0]
+ 			  , [0, 0, 0]
+ 			  , [0, 0, 0]];
+
+//test
+pinGrid[1][2] = 3;
+console.log(pinGrid);
 
 wpi.setup('wpi');
 
 wpi.pinMode(row1Pin, wpi.INPUT);
 wpi.pullUpDnControl(row1Pin, wpi.PUD_UP);
 
+wpi.pinMode(row2Pin, wpi.INPUT);
+wpi.pullUpDnControl(row2Pin, wpi.PUD_UP);
+
+wpi.pinMode(row3Pin, wpi.INPUT);
+wpi.pullUpDnControl(row3Pin, wpi.PUD_UP);
+
+wpi.pinMode(row4Pin, wpi.INPUT);
+wpi.pullUpDnControl(row4Pin, wpi.PUD_UP);
+
+wpi.pinMode(col1Pin, wpi.INPUT);
+wpi.pullUpDnControl(col1Pin, wpi.PUD_UP);
+
+wpi.pinMode(col2Pin, wpi.INPUT);
+wpi.pullUpDnControl(col2Pin, wpi.PUD_UP);
+
+wpi.pinMode(col3Pin, wpi.INPUT);
+wpi.pullUpDnControl(col3Pin, wpi.PUD_UP);
+
+wpi.pinMode(hangupPin, wpi.INPUT);
+wpi.pullUpDnControl(hangupPin, wpi.PUD_UP);
+
+
+
 
 wpi.wiringPiISR(row1Pin, wpi.INT_EDGE_FALLING, function() {
-
        	console.log("row1");
-
 });
