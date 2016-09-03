79750ba6c3083d109bc4a266f3a6a13fb21548ad
Ashley Holtgraver <aholtgraver@gmail.com>
2016-09-03 13:58:56 -0400

    starting on touchtone pin mappings

--- raspi/touchtone.js
+++ raspi/touchtone.js
@@ -1,34 +1,17 @@
 var wpi = require('wiring-pi');
 
 //// GPIO pin of the button
-var configPin =8;
+// check gpio readall in the cli to check for the wPI numbers
+var row1Pin = 9
+  , row2Pin = 7
+  , row3Pin = 12
+  , row4Pin = 3
+  , col1Pin = 0
+  , col2Pin = 2
+  , col3Pin = 13
+  , hangupPin = 8;
 
-var mySound;
 
-var soundPath = "feed/sounds/";
-// need to make sure to grab this on every cradle pickup... 
-//    or maybe just have the app restart once a day?
-//var allsounds = fs.readdirSync(soundPath);
-//console.log(allsounds);
-
-//var test = new Sound('feed/sounds/new_1472780372380.wav')
-var test = new Sound('test.wav');
-
-//for (var i=0; i<allsounds.length; i++){
-//	if (allsounds[i].substring(0,4) == 'new_') {
-//		//new file - rename, and play it!
-//		var newname = allsounds[i].substring(4);
-//	//	fs.rename(soundPath + allsounds[i], soundPath + newname);
-//		console.log("play newname "+soundPath + allsounds[i]);
-//		mySound = new Sound(soundPath + allsounds[i]);
-//		mySound.play();
-//		break;
-//	}
-//}
-
-//console.log("started, playing "+mySound);
-test.play();
-//
 
 wpi.setup('wpi');
 
@@ -36,9 +19,8 @@ wpi.pinMode(configPin, wpi.INPUT);
 wpi.pullUpDnControl(configPin, wpi.PUD_UP);
 
 
-wpi.wiringPiISR(configPin, wpi.INT_EDGE_BOTH, function() {
-//  if (wpi.digitalRead(configPin)) {
-       	console.log("push");
-       	test.play();
- // }
+wpi.wiringPiISR(row1Pin, wpi.INT_EDGE_FALLING, function() {
+
+       	console.log("row1");
+
 });
