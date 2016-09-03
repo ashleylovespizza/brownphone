776ffe0995f8f97a0c10a920442ce02e05985f36
Ashley Holtgraver <aholtgraver@gmail.com>
2016-09-03 16:15:07 -0400

    perhaps overcomplicating things...

--- raspi/touchtone.js
+++ raspi/touchtone.js
@@ -11,19 +11,38 @@ var row1Pin = 9
   , col3Pin = 13
   , hangupPin = 8;
 
+// this seems insane but bear with me...
+// pinGrid is laid out so each pin location is represented by a [row, col] boolean set
+// third item is unix tiem of most recent button press
+// pressing a number triggers both a row and col press
+// so actual number pressed will be the only one with [true, true]
+var pinGrid = [ [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]];
 
-var pinGrid = [ [0, 0, 0]
- 			  , [0, 0, 0]
- 			  , [0, 0, 0]
- 			  , [0, 0, 0]];
-
-
+var row1Last=0
+  , row2Last=0
+  , row3Last=0
+  , row4Last=0
+  , col1Last=0
+  , col2Last=0
+  , col3Last=0;
 // -1 = *
 // -2 = #
 var numPad =  [ [1, 2, 3]
  			  , [4, 5, 6]
  			  , [7, 8, 9]
  			  , [-1, 0, -2]];
+
+var deltaVal = 1;
+var lastPressTime = 0;
+var timeThreshhold = 70;
+
+//TEST
+// pinGrid[2][1]++;
+// console.log(pinGrid);
+// console.log(numPad[2][1]);
 
 wpi.setup('wpi');
 
@@ -52,31 +71,29 @@ wpi.pinMode(hangupPin, wpi.INPUT);
 wpi.pullUpDnControl(hangupPin, wpi.PUD_UP);
 
 function resetPinGrid() {
-    pinGrid = [ [0, 0, 0]
- 			  , [0, 0, 0]
- 			  , [0, 0, 0]
- 			  , [0, 0, 0]];
+	pinGrid = [ [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]
+ 			  , [[false, false, 0], [false,false, 0], [false,false, 0]]];
 	console.log("---------------------------");
 }
+
+
 function checkForPress() {
-	var maxVal = 1;
-	var maxLocation = [];
+	var pressLocation = [];
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 3; j++) {
-			if (pinGrid[i][j] > maxVal) {
-				maxVal = pinGrid[i][j];
-				maxLocation = [i, j];
+			if (pinGrid[i][j][0] && pinGrid[i][j][1]) {
+				pressLocation = [i, j];
 			}
 		}
 	}
-//	console.log(maxLocation);
 
-	if (maxLocation.length > 0) {
-		console.log(maxLocation);
-		console.log("The number "+numPad[maxLocation[0]][maxLocation[1]]+" has been pressed!");
+	if (pressLocation.length > 0) {
+		console.log(pressLocation);
+		console.log("The number "+numPad[pressLocation[0]][pressLocation[1]]+" has been pressed!");
 		resetPinGrid();
 	} else {
-		console.log("errr");
 	}
 }
 
@@ -84,52 +101,142 @@ function checkForPress() {
 
 
 
-wpi.wiringPiISR(row1Pin, wpi.INT_EDGE_FALLING, function() {
-		if (wpi.digitalRead(row1Pin)) {
-			console.log("row 1: ");
-	       	pinGrid[0][0]++;
-	       	pinGrid[0][1]++;
-	       	pinGrid[0][2]++;
+wpi.wiringPiISR(row1Pin, wpi.INT_EDGE_RISING, function(delta) {
+	if( wpi.digitalRead(row1Pin) && delta > deltaVal){
+		
+
+		var now = Number(Date.now());
+		console.log("row 1: " + String(now - pinGrid[0][0][2])+", "+(now - pinGrid[0][1][2])+", "+(now - pinGrid[0][2][2]));
+		console.log(" ")
+
+		if (   now - pinGrid[0][0][2] > timeThreshhold
+			&& now - pinGrid[0][1][2] > timeThreshhold
+			&& now - pinGrid[0][2][2] > timeThreshhold) {
+			console.log("REAL PRESS!");
+
+	       	pinGrid[0][0][0] = true;
+	       	pinGrid[0][1][0] = true;
+	       	pinGrid[0][2][0] = true;
+	       	checkForPress();
+	    }
+
+	    pinGrid[0][0][2] = now;
+	    pinGrid[0][1][2] = now;
+	    pinGrid[0][2][2] = now;
+	}
+});
+wpi.wiringPiISR(row2Pin, wpi.INT_EDGE_RISING, function(delta) {
+		if (wpi.digitalRead(row2Pin) && delta > deltaVal){
+			console.log("row 2: "  );
+			console.log(" ")
+
+
+			var now = Date.now();
+			if (   now - pinGrid[1][0][2] > timeThreshhold
+				&& now - pinGrid[1][1][2] > timeThreshhold
+				&& now - pinGrid[1][2][2] > timeThreshhold) {
+			console.log("REAL PRESS!");
+		       	pinGrid[1][0][0] = true;
+		       	pinGrid[1][1][0] = true;
+		       	pinGrid[1][2][0] = true;
+
+
+		       	checkForPress();
+			}
+		    pinGrid[1][0][0] = now;
+		    pinGrid[1][1][0] = now;
+		    pinGrid[1][2][0] = now;
+
+		}		
+});
+
+
+wpi.wiringPiISR(col1Pin, wpi.INT_EDGE_RISING, function(delta) {
+	if (wpi.digitalRead(col1Pin) && delta > deltaVal){
+		var now = Date.now();
+		console.log("col 1 : "+ String(now - pinGrid[0][0][2]) + ", "+ (now - pinGrid[1][0][2]) +" , "+(now - pinGrid[2][0][2]) +", "+(now - pinGrid[3][0][2]) );
+		console.log(" ")
+
+		if (   now - pinGrid[0][0][2] > timeThreshhold
+			&& now - pinGrid[1][0][2] > timeThreshhold
+			&& now - pinGrid[2][0][2] > timeThreshhold
+			&& now - pinGrid[3][0][2] > timeThreshhold) {
+			console.log("REAL PRESS!");
+
+	       	pinGrid[0][0][1] = true;
+	       	pinGrid[1][0][1] = true;
+	       	pinGrid[2][0][1] = true;
+	       	pinGrid[3][0][1] = true;
+
+	     	checkForPress();
+		}
+
+	    pinGrid[0][0][2]= now;
+		pinGrid[1][0][2]= now;
+		pinGrid[2][0][2]= now;
+		pinGrid[3][0][2] = now;
+
+
+	}
+});
+wpi.wiringPiISR(col2Pin, wpi.INT_EDGE_RISING, function(delta) {
+	if ( wpi.digitalRead(col2Pin) ){
+		var now = Date.now();
+		console.log("col 2: " + String(now - pinGrid[0][1][2])  +", "+ (now - pinGrid[1][1][2])  +", "+ (now - pinGrid[2][1][2])  + ", "+ (now - pinGrid[3][1][2]) );
+		console.log(" ")
+
+		if (   now - pinGrid[0][1][2] > timeThreshhold
+			&& now - pinGrid[1][1][2] > timeThreshhold
+			&& now - pinGrid[2][1][2] > timeThreshhold
+			&& now - pinGrid[3][1][2] > timeThreshhold) {
+			console.log("REAL PRESS!");
+
+  		pinGrid[0][1][1] = true;
+       	pinGrid[1][1][1] = true;
+       	pinGrid[2][1][1] = true;
+       	pinGrid[3][1][1] = true;
+
+      	checkForPress();
+
+
+		}
+	    pinGrid[0][1][2]= now;
+		pinGrid[1][1][2]= now;
+		pinGrid[2][1][2]= now;
+		pinGrid[3][1][2] = now;
+	}
+	
+});
+
+wpi.wiringPiISR(col3Pin, wpi.INT_EDGE_RISING, function(delta) {
+	if (wpi.digitalRead(col3Pin)) {
+		console.log("col 3: ");
+		
+		var now = Date.now();
+		if (   now - pinGrid[0][2][2] > timeThreshhold
+			&& now - pinGrid[1][2][2] > timeThreshhold
+			&& now - pinGrid[2][2][2] > timeThreshhold
+			&& now - pinGrid[3][2][2] > timeThreshhold) {
+			console.log("REAL PRESS!");
+
+
+			console.log(" ")
+	       	pinGrid[0][2][1] = true;
+	       	pinGrid[1][2][1] = true;
+	       	pinGrid[2][2][1] = true;
+	       	pinGrid[3][2][1] = true;
+
+
 	       	checkForPress();
 		}
 
-});
 
+	    var now = Date.now();
+	    pinGrid[0][2][2]= now;
+		pinGrid[1][2][2]= now;
+		pinGrid[2][2][2]= now;
+		pinGrid[3][2][2] = now;
 
-
-wpi.wiringPiISR(col1Pin, wpi.INT_EDGE_FALLING, function() {
-	if( wpi.digitalRead(col1Pin) ){
-		console.log("col 1 ");
-       	pinGrid[0][0]++;
-       	pinGrid[1][0]++;
-       	pinGrid[2][0]++;
-       	pinGrid[3][0]++;
-       	checkForPress();
-	}
-	
-});
-
-
-wpi.wiringPiISR(col2Pin, wpi.INT_EDGE_FALLING, function() {
-	if ( wpi.digitalRead(col2Pin) ){
-		console.log("col 2: ");
-       	pinGrid[0][1]++;
-       	pinGrid[1][1]++;
-       	pinGrid[2][1]++;
-       	pinGrid[3][1]++;
-       	checkForPress();
-	}
-	
-});
-
-wpi.wiringPiISR(col3Pin, wpi.INT_EDGE_FALLING, function() {
-	if (wpi.digitalRead(col3Pin)) {
-		console.log("col 3: ");
-       	pinGrid[0][2]++;
-       	pinGrid[1][2]++;
-       	pinGrid[2][2]++;
-       	pinGrid[3][2]++;
-       	checkForPress();
 	}
 	
 });
